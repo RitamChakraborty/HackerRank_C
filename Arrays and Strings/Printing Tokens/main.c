ff091564daa29563b0be23d75988a382eacd9cd1
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2020-07-09 19:49:03 +0530

    Solve Printing Tokens

---
+++ Arrays and Strings/Printing Tokens/main.c
@@ -0,0 +1,23 @@
+// https://www.hackerrank.com/challenges/printing-tokens-/problem
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main() {
+
+    char *s;
+    s = malloc(1024 * sizeof(char));
+    scanf("%[^\n]", s);
+    s = realloc(s, strlen(s) + 1);
+    
+    for (int i = 0; i < strlen(s); ++i) {
+        if (s[i] == ' ') {
+            printf("\n");
+        } else {
+            printf("%c", s[i]);
+        }
+    }
+
+    return 0;
+}
